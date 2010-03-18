d48609e19e627095d6a2601fde035c9e68e0ae31
Paul Robins <paul@wza.us>
2010-03-18 04:40:59 +0000

    Updated readme Script will now work with netxml/gpsxml Handled cards which do not report signal strength

--- kismet2kml.pl
+++ kismet2kml.pl
@@ -27,14 +27,31 @@ use Math::NumberCruncher;
 use XML::Simple;
 use XML::Generator;
 
-my $prefix  = $ARGV[0]  or die "Please specify a prefix for gps and xml files";
-(-r "$prefix.gps" && -r "$prefix.xml") or die "Either the gps or xml file is unreadable";
-
 my ($minpower,$maxpower)  = undef;
+# Older kismet files set signal to 0 if the card does not provide it
+# newer files appear to not set the signal at all
+my $seensignal  = 0;
 my %waps;
+
+my $prefix  = $ARGV[0]  or die "Please specify a prefix for gps and xml files";
+
+my ($gpsin,$xmlin);
+if (-r "$prefix.gps" && -r "$prefix.xml") {
+  $gpsin = XMLin("$prefix.gps") or die "Failed to parse gps input file";
+  $xmlin = XMLin("$prefix.xml") or die "Failed to parse xml input file";
+  parse_gps();
+} elsif (-r "$prefix.gpsxml" && -r "$prefix.netxml") {
+  $gpsin = XMLin("$prefix.gpsxml") or die "Failed to parse gps input file";
+  $xmlin = XMLin("$prefix.netxml") or die "Failed to parse xml input file";
+  parse_gpsxml();
+} else {
+  die "Either the gps or xml file is unreadable";
+}
 
 sub norm_power {
   my $in  = shift;
+
+  return 1 unless ($seensignal);
 
   $in     += abs($minpower);
   $in     /= ($maxpower+abs($minpower))/5;
@@ -43,32 +60,60 @@ sub norm_power {
   return $in;
 }
 
-my $gpsin = XMLin("$prefix.gps") or die "Failed to parse gps input file";
-my $xmlin = XMLin("$prefix.xml") or die "Failed to parse xml input file";
-
-for my $network (@{$xmlin->{'wireless-network'}}) {
-  my $wap = {};
-  $wap->{SSID}    = $network->{SSID};
-  $wap->{BSSID}   = $network->{BSSID};
-  $wap->{Channel} = $network->{channel};
-  if (ref($network->{encryption})  ne 'ARRAY') {
-    $wap->{Encryption}  = $network->{encryption};
-  } else {
-    $wap->{Encryption}  = join(', ',@{$network->{encryption}});
-  }
-
-  if ($network->{'wireless-client'}) {
-    if (ref($network->{'wireless-client'})  eq 'HASH') {
-      $wap->{Clients} = 1;
+sub parse_gps {
+  for my $network (@{$xmlin->{'wireless-network'}}) {
+    my $wap = {};
+    $wap->{ESSID}   = $network->{SSID};
+    $wap->{BSSID}   = $network->{BSSID};
+    $wap->{Channel} = $network->{channel};
+    if (ref($network->{encryption})  ne 'ARRAY') {
+      $wap->{Encryption}  = $network->{encryption};
     } else {
-      $wap->{Clients} = scalar @{$network->{'wireless-client'}};
+      $wap->{Encryption}  = join(', ',@{$network->{encryption}});
     }
+  
+    if ($network->{'wireless-client'}) {
+      if (ref($network->{'wireless-client'})  eq 'HASH') {
+        $wap->{Clients} = 1;
+      } else {
+        $wap->{Clients} = scalar @{$network->{'wireless-client'}};
+      }
+    }
+    $wap->{Clients} //= 0;
+  
+    $waps{$network->{BSSID}}  = {desc => $wap};
   }
-  $wap->{Clients} //= 0;
-
-  $waps{$network->{BSSID}}  = {desc => $wap};
 }
 
+sub parse_gpsxml {
+  for my $network (@{$xmlin->{'wireless-network'}}) {
+    next unless (defined($network->{SSID}));
+
+    my $wap = {};
+    $wap->{ESSID}   = $network->{SSID}->{essid}->{content} if ($network->{SSID}->{essid}->{content});
+    $wap->{BSSID}   = $network->{BSSID};
+    $wap->{Channel} = $network->{channel};
+
+    if (ref($network->{SSID}->{encryption}) ne 'ARRAY') {
+      $wap->{Encryption}  = $network->{SSID}->{encryption};
+    } else {
+      $wap->{Encryption}  = join(', ',@{$network->{SSID}->{encryption}});
+    }
+
+    if ($network->{'wireless-client'}) {
+      if (ref($network->{'wireless-client'})  eq 'HASH') {
+        $wap->{Clients} = 1;
+      } else {
+        $wap->{Clients} = scalar @{$network->{'wireless-client'}};
+      }
+    }
+    $wap->{Clients} //= 0;
+  
+    $waps{$network->{BSSID}}  = {desc => $wap};
+  }
+}
+
+print STDERR "Beginning GPS point scan\n";
 for my $point (@{$gpsin->{'gps-point'}}) {
   # Ignore any points without a full 3d GPS fix
   next unless ($point->{fix}  == 3);
@@ -76,9 +121,13 @@ for my $point (@{$gpsin->{'gps-point'}}) {
   next unless ($waps{$point->{bssid}});
 
   my $bssid = $point->{bssid};
-  
-  $minpower = $point->{signal} unless ($point->{signal} == 0 || defined($minpower) && $minpower <= $point->{signal});
-  $maxpower = $point->{signal} unless ($point->{signal} == 0 || defined($maxpower) && $maxpower >= $point->{signal});
+
+  $seensignal = 1 if ($point->{signal} && $point->{signal}  != 0);
+
+  if ($point->{signal}) {
+    $minpower = $point->{signal} unless ($point->{signal} == 0 || defined($minpower) && $minpower <= $point->{signal});
+    $maxpower = $point->{signal} unless ($point->{signal} == 0 || defined($maxpower) && $maxpower >= $point->{signal});
+  }
 
   $waps{$bssid} = {
     points  => [],
@@ -88,6 +137,7 @@ for my $point (@{$gpsin->{'gps-point'}}) {
 
   push @{$waps{$bssid}->{points}}, $point;
 }
+print STDERR "Finished GPS point scan\n";
 
 # This is the path, not a genuine SSID
 my $path  = delete($waps{'GP:SD:TR:AC:KL:OG'});
@@ -98,46 +148,39 @@ my $gen = XML::Generator->new(
 );
 
 my @elements;
+print STDERR "Beginning WAP calculations\n";
 for my $wap (sort {$waps{$a}->{time} <=> $waps{$b}->{time}} keys %waps) {
-  my @points  = sort {$a->{lat} <=> $b->{lat} ||
+  my @sorted  = sort {$a->{lat} <=> $b->{lat} ||
                       $a->{lon} <=> $b->{lon}} @{$waps{$wap}->{points}};
 
-  my @sorted;
-  my %pointrefs;
-  for my $point (@points) {
-    my $coordref  = [$point->{lat},$point->{lon}];
-
-    if (@sorted && $sorted[$#sorted]->[0] == $point->{lat} && $sorted[$#sorted]->[1] == $point->{lon}) {
+  my @points;
+  for my $point (@sorted) {
+    if ($points[$#points] &&  $points[$#points]->{lat} == $point->{lat} &&
+                              $points[$#points]->{lon} == $point->{lon}) {
+      next unless ($point->{signal} && $point->{signal} != 0);
       # Multiple points at the same location may have different power levels
-      if ($pointrefs{$point}->{signals}) {
-        push @{$sorted[$#sorted]->{signals}}, $point->{signal};
-        $sorted[$#sorted]->{signal} = Math::NumberCruncher::Median($sorted[$#sorted]->{signals});
-      } else {
-        $pointrefs{$sorted[$#sorted]}->{signals} = [$pointrefs{$sorted[$#sorted]}->{signal}];
-      }
+      push @{$points[$#points]->{signals}}, $point->{signal};
+      $points[$#points]->{signal} = Math::NumberCruncher::Median($points[$#points]->{signals});
       next;
     };
 
-    push @sorted,$coordref;
-    $pointrefs{$coordref} = $point;
+    $point->{signals} = [$point->{signal}];
+    push @points,$point;
   }
 
-  my $hull  = convex_hull(\@sorted);
-
-  my $hullpoints;
   my @geometry;
-  for my $point (@{$hull}) {
+  for my $point (@points) {
     next unless ($point);
     push @geometry, $gen->Point(
       $gen->extrude('1'),
       $gen->altitudeMode('relativeToGround'),
-      $gen->coordinates($pointrefs{$point}->{lon} ."," . $pointrefs{$point}->{lat} . "," . norm_power($pointrefs{$point}->{signal})),
+      $gen->coordinates($point->{lon} .",". $point->{lat} .",". norm_power($point->{signal})),
     );
   }
 
   my $id;
-  if ($waps{$wap}->{desc}->{SSID}) {
-    $id = $waps{$wap}->{desc}->{SSID};
+  if ($waps{$wap}->{desc}->{ESSID}) {
+    $id = $waps{$wap}->{desc}->{ESSID};
   } else {
     $id = $wap;
   }
@@ -156,10 +199,12 @@ for my $wap (sort {$waps{$a}->{time} <=> $waps{$b}->{time}} keys %waps) {
 
   push @elements, $xml;
 }
-
+print STDERR "Finished WAP calculations\n";
+print STDERR "Printing KML\n";
 print '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
 print $gen->kml(['http://www.opengis.net/kml/2.2'],
   $gen->Folder(
     $gen->name('Kismet GPS'),
     @elements,
   )) . "\n";
+print STDERR "Done\n";
